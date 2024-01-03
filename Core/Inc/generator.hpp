9660237221fbb71d528d87147d7830e1b91d44db
Justin_Lau <justin610810@outlook.com>
2024-01-03 14:28:19 +0800

    Complete comment of functions of class generator

--- Core/Inc/generator.hpp
+++ Core/Inc/generator.hpp
@@ -21,18 +21,75 @@ class Generator {
     Generator() = default;
     ~Generator();
 
+    /**
+     * @brief Start the Program
+     *
+     */
     void start();
+
+    /**
+     * @brief Input data by user and output CSV file
+     *
+     * @param current
+     */
     void insert_csv(Transcript* current);
+
+    /**
+     * @brief Input data from CSV file
+     *
+     * @param current
+     */
     void read_csv(Transcript* current);
+
+    /**
+     * @brief Modify data that inside CSV file
+     *
+     * @param current
+     */
     void modify_csv(Transcript* current);
+
+    /**
+     * @brief Generate transcript into terminal
+     *
+     * @param current
+     */
     void print_all(Transcript* current);
+
+    /**
+     * @brief Edit another transcript
+     *
+     * @param current
+     */
     void restart(Transcript* current);
+
+    /**
+     * @brief Release all dellocated memory
+     *
+     * @param current
+     */
     void end(Transcript* current);
 
+    /**
+     * @brief Return number of transcript
+     *
+     * @return int
+     */
     int get_num_transcript() const;
 
+    /**
+     * @brief Insert other transcript
+     *
+     * @param other_transcript
+     */
     void insert_transcript(Transcript* other_transcript);
+
+    /**
+     * @brief Remove transcript according to given index
+     *
+     * @param index
+     */
     void remove_transcript(int index);
+
     void clear_nullptr();
 };
 
