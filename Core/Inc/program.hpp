c041b3f3663d972333860bbdfd4b9b2d1242f133
Justin_Lau <justin610810@outlook.com>
2024-01-04 17:02:28 +0800

    Add comment to functions of class Program

--- Core/Inc/program.hpp
+++ Core/Inc/program.hpp
@@ -15,10 +15,32 @@ class Program {
     Program(const Program& other);
     ~Program() = default;
 
+    /**
+     * @brief Get the name object
+     * 
+     * @return string 
+     */
     string get_name() const;
+
+    /**
+     * @brief Get the change_date object
+     * 
+     * @return string 
+     */
     string get_change_date() const;
 
+    /**
+     * @brief Change name to other_name
+     * 
+     * @param other_name 
+     */
     void change_name(const string& other_name);
+
+    /**
+     * @brief Change change_date to other_change_date
+     * 
+     * @param other_change_date 
+     */
     void change_change_date(const string& other_change_date);
 
     Program& operator=(const Program& other);
@@ -38,8 +60,18 @@ class Major : public Program {
     Major(const Major& other);
     ~Major() = default;
 
+    /**
+     * @brief Get the name_major object
+     * 
+     * @return string 
+     */
     string get_name_major() const;
 
+    /**
+     * @brief Change name_major to other_name_major
+     * 
+     * @param other_name_major 
+     */
     void change_name_major(const string& other_name_major);
 
     Major& operator=(const Major& other);
@@ -59,8 +91,18 @@ class Minor : public Program {
     Minor(const Minor& other);
     ~Minor() = default;
 
+    /**
+     * @brief Get the name_minor object
+     * 
+     * @return string 
+     */
     string get_name_minor() const;
 
+    /**
+     * @brief Change name_minor to other_name_minor
+     * 
+     * @param other_name_minor 
+     */
     void change_name_minor(const string& other_name_minor);
 
     Minor& operator=(const Minor& other);
