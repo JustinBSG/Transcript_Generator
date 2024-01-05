7fd3b7567bbf1e5a800269b7fbadf8819290208e
Justin_Lau <justin610810@outlook.com>
2024-01-05 15:18:14 +0800

    add comment to functions to class semester

--- Core/Inc/semester.hpp
+++ Core/Inc/semester.hpp
@@ -23,18 +23,64 @@ class Semester {
              int total_num_courses = 0);
     ~Semester() = default;
 
+    /**
+     * @brief Get the period object
+     * 
+     * @return string 
+     */
     string get_period() const;
+
+    /**
+     * @brief Get the tga object
+     * 
+     * @return float 
+     */
     float get_tga() const;
+
+    /**
+     * @brief Get the total_num_credits object
+     * 
+     * @return int 
+     */
     int get_total_num_credits() const;
+
+    /**
+     * @brief Get the total_num_courses object
+     * 
+     * @return int 
+     */
     int get_total_num_courses() const;
+
+    /**
+     * @brief Get the courses object
+     * 
+     * @return BST<Course>& 
+     */
     BST<Course>& get_courses();
 
+    /**
+     * @brief Change period to other_period
+     * 
+     * @param other_period 
+     */
     void change_period(const string& other_period);
+
     void calculate_tga();
     void calculate_total_num_credits();
     void update_total_num_courses();
 
+    /**
+     * @brief Insert other Course object to BST<Course>
+     * 
+     * @param other 
+     */
     void insert_course(const Course& other);
+
+    /**
+     * @brief Remove course with same code
+     * 
+     * @param code 
+     */
     void remove_course(const string& code);
 
     Semester& operator=(const Semester& other);
