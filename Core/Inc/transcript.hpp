ffa9f131131d5deb584b12a10d5509b85d7e7ec5
Justin_Lau <justin610810@outlook.com>
2024-01-09 12:30:47 +0800

    Add comment to functions of class transript

--- Core/Inc/transcript.hpp
+++ Core/Inc/transcript.hpp
@@ -19,17 +19,71 @@ class Transcript {
     Transcript(Student *user = nullptr, Professor *advisor = nullptr);
     ~Transcript();
 
+    /**
+     * @brief Get the user object
+     *
+     * @return Student*
+     */
     Student *get_student() const;
+
+    /**
+     * @brief Get the advisor object
+     *
+     * @return Professor*
+     */
     Professor *get_professor() const;
+
+    /**
+     * @brief Get the semesters object
+     *
+     * @return BST<Semester>*
+     */
     BST<Semester> *get_semesters() const;
+
+    /**
+     * @brief Get the print_date object
+     *
+     * @return string
+     */
     string get_print_date() const;
 
+    /**
+     * @brief Insert other_student
+     *
+     * @param other_student
+     */
     void insert_student(Student *other_student);
+
+    /**
+     * @brief Insert other_professor
+     *
+     * @param other_professor
+     */
     void insert_professor(Professor *other_professor);
+
+    /**
+     * @brief Insert other_semester into semesters
+     *
+     * @param other_semester
+     */
     void insert_semester(BST<Semester> *other_semester);
+
+    /**
+     * @brief Update print_date
+     *
+     */
     void update_print_date();
 
+    /**
+     * @brief Update CGA inside student class object
+     *
+     */
     void calculate_CGA();
+
+    /**
+     * @brief Updata MCGA inside student class object
+     *
+     */
     void calculate_MCGA();
 
     void print() const;
