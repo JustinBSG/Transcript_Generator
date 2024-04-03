d545b08e17918b2e624028afcb067de9081ddf9d
JustinBSG <justin610810@outlook.com>
2024-04-03 12:51:27 +0800

    Complete input data of student in insert_data() in generator.cpp

--- after_refactor/Core/Src/generator.cpp
+++ after_refactor/Core/Src/generator.cpp
@@ -81,7 +81,7 @@ void Generator::insert_data(Transcript*& current) {
    *      - list out all choices and let user to choose
    *    2.7. BST<Major> majors
    *      2.7.1. string program_name (Bachelor of Engineering)
-   *      2.7.2. string change_date (2022-23 Fall) 
+   *      2.7.2. string change_date (2022-23 Fall)
    *      2.7.3. string major_name (Electronic Engineering)
    *      - 1st time, just input 2.7.1. and 2.7.3., 2.7.2. is same as admit_date
    *      - next time, ask user whether he has majors left
@@ -99,8 +99,8 @@ void Generator::insert_data(Transcript*& current) {
    *    3.1. string name (CHAN Da Man)
    *    3.2. string department (CSE)
    * 4. input data in Semester
-   *    4.1. According to year, generating that 4*year numbers of semester with period and save them into BST<Semester*> semesters
-   *    4.2. ask user whether he has registered course in that semester
+   *    4.1. According to year, generating that 4*year numbers of semester with period and save them
+   * into BST<Semester*> semesters 4.2. ask user whether he has registered course in that semester
    *      4.2.1. yes then
    *        4.2.1.1. ask user whether he has input data of all courses in that semester
    *          - no then go to 4.2.1.2
@@ -114,8 +114,45 @@ void Generator::insert_data(Transcript*& current) {
    *    4.3. check whether all semesters are inputed data
    *      4.3.1. yes then return
    *      4.3.2. no then go to 4.2.
-   * 
+   *
    */
+
+  if (current != nullptr) {
+    std::cout << "You should save the current transcript first before inputting data." << std::endl;
+    return;
+  }
+
+  std::cout << "First, please input the information about you." << std::endl;
+  std::cout << "Please input your full name: ";
+  std::string student_name;
+  getline(std::cin, student_name);
+  std::cout << "Please input your admit date(e.g. 1 September 2020): ";
+  std::string student_admit_date;
+  getline(std::cin, student_admit_date);
+  std::cout << "Please input which department that you belong to(e.g. CSE)" << std::endl;
+  std::cout << "If you haven't had a major yet, please input the school that you belongs to(e.g. SENG): ";
+  std::string student_department;
+  getline(std::cin, student_department);
+  std::cout << "Please input your student ID card number: ";
+  int student_id;
+  std::cin >> student_id;
+  std::cout << "Please input your years of study: ";
+  int student_year;
+  std::cin >> student_year;
+  std::cout << "Please choose your status of study:" << std::endl;
+  std::cout << "1. Active in Program" << std::endl;
+  std::cout << "2. Withdraw Program" << std::endl;
+  std::cout << "3. Suspend Program" << std::endl;
+  std::cout << "Please input your choose in number: ";
+  int student_status;
+  std::cin >> student_status;
+  while (student_status > 3 || student_status < 1) {
+    std::cout << "Please input a valid number: ";
+    std::cin >> student_status;
+  }
+  std::cin.ignore();
+  Student* temp_student;
+  temp_student = new Student{student_name, student_admit_date, student_department, student_id, student_year, static_cast<Status_Program>(student_status)};
 }
 
 void Generator::read_csv(Transcript*& current) {}
